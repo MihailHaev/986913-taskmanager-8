cb91fb9759b18524a38fbd252128379ec2989e5b
MihailHaev <khriokhriokh@icloud.com>
2019-02-26 13:10:00 +0300

    Webpack created

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,37 @@
-const webpack = require(`webpack`);
 const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+const CopyPlugin = require(`copy-webpack-plugin`);
 
 module.exports = {
-  node: `development`,
+  mode: `development`,
   entry: `./src/main.js`,
   output: {
-    path: path.join(__dirname, `public`),
     filename: `bundle.js`,
+    path: path.join(__dirname, `build`)
   },
-  devtool: `source-map`
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `build`), // Откуда поднимаем сервер
+    publicPath: `http://localhost:8080/`,
+    hot: true, // livereload
+    inline: true, // alllivereload
+    compress: true // сжатие
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: `babel-loader`
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./public/index.html`
+    }),
+    new CopyPlugin([
+      { from: `public/css`, to: `css` },
+      { from: `public/fonts`, to: `fonts` },
+      { from: `public/img`, to: `img` },
+      { from: `public/index.html`, to: `index.html` },
+    ]),
+  ]
 };
